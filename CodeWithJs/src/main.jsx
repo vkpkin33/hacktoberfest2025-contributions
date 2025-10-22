5f581b1785e720a570590e3ea945c1aff8c05dc7
Mayank-Kumar-Maurya <mayankmaurya2122@gmail.com>
2025-10-22 22:14:01 +0530

    Added the backend files and connections with frontend with functionalities and their working

--- CodeWithJs/src/main.jsx
+++ CodeWithJs/src/main.jsx
@@ -7,12 +7,15 @@ import 'bootstrap/dist/css/bootstrap.min.css'
 import 'animate.css'
 import './index.css'
 import App from './App.jsx'
+import AuthContextProvider from './Context/AuthContextProvider.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <Provider store={store}>
       <BrowserRouter>
-        <App />
+        <AuthContextProvider>
+          <App />
+        </AuthContextProvider>
       </BrowserRouter>
     </Provider>
   </StrictMode>,
